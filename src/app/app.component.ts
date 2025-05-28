a3e4fd1233ddbf579711692ac9334f11710d9ecf
Sittinon <nice23615@gmail.com>
2025-05-28 11:36:56 +0700

    feat: Add audio playback functionality with mute toggle in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component, AfterViewInit, HostListener } from '@angular/core';
+import { Component, AfterViewInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router'; // เพิ่มบรรทัดนี้
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterModule] // เพิ่มบรรทัดนี้
+  imports: [RouterModule, CommonModule] // เพิ่มบรรทัดนี้
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  isMuted = false;
+
+  @ViewChild('bgm') bgmRef!: ElementRef<HTMLAudioElement>;
+
+  toggleMute() {
+    this.isMuted = !this.isMuted;
+  }
+
+  ngAfterViewInit() {
+    if (this.bgmRef?.nativeElement) {
+      this.bgmRef.nativeElement.volume = 0.18; // ปรับเสียงเบาๆ นุ่มๆ
+    }
+  }
+}
 
